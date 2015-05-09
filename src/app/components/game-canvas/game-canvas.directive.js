5842d550851f1851e9c93e15a7b9bb90b0c006e1
shian <shian5@gmail.com>
2015-05-09 22:28:15 +0800

    update layout

--- src/app/components/game-canvas/game-canvas.directive.js
+++ src/app/components/game-canvas/game-canvas.directive.js
@@ -5,7 +5,7 @@ angular.module('stank.game', [])
     var linkFn = function (scope, ele, attrs) {
       console.log("linkFn");
       var height  = parseInt(ele.css('height'), 10),
-        width   = parseInt(ele.css('width'), 10);
+          width   = parseInt(ele.css('width'), 10);
       scope.game = new Phaser.Game(width, height, Phaser.AUTO, 'game-canvas');
       console.log("game engine create");
       scope.onInit({game: scope.game});
